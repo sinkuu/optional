34886e4a3b401b9115549080fbde581e3b9b315c
sinkuu <sinkuupump@gmail.com>
2014-12-31 22:15:29 +0900

    Add Optional chaining

--- source/optional.d
+++ source/optional.d
@@ -223,6 +223,54 @@ unittest
 	assert(optional!int().empty);
 }
 
+
+import std.traits : isInstanceOf;
+// TODO: better name
+auto bind(alias fun, Opt)(Opt opt) if (isInstanceOf!(Optional, Opt))
+{
+	import std.functional : unaryFun;
+
+	static if (is(typeof(unaryFun!fun) == void))
+	{
+		alias ufun = unaryFun!fun;
+		alias fun_ = ufun!(typeof(opt.get()));
+	}
+	else
+	{
+		alias fun_ = unaryFun!fun;
+	}
+
+	return opt.empty ?
+		Optional!(typeof(fun_(opt.get).get()))() : fun_(opt.get);
+}
+
+///
+unittest
+{
+	Optional!int func1(int x)
+	{
+		return x == 0 ?
+			optional!int() : optional(x);
+	}
+
+	Optional!int func2(int y)
+	{
+		return y == 10 ?
+			optional!int() : optional(y);
+	}
+
+	assert(func1(0) .bind!func2.empty);
+	assert(func1(5) .bind!func2.get == 5);
+	assert(func1(10).bind!func2.empty);
+}
+
+@safe pure nothrow @nogc
+unittest
+{
+	assert(optional(10).bind!(x => optional(x == 10)).get);
+}
+
+
 auto optSwitch(alias existFun, alias emptyFun = {}, T)(Optional!T opt)
 {
 	import std.functional : unaryFun;
